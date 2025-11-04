0a447f11e713a604697e0024ad37b7ee18e3cf75
gkits <gkitsikoudis@gmx.de>
2025-11-04 13:17:11 +0100

    fix(nvim): correctly apply new structure to dap

--- nvim/.config/nvim/lua/gkits/plugins/dap.lua
+++ nvim/.config/nvim/lua/gkits/plugins/dap.lua
@@ -1,92 +1,57 @@
 return {
     "mfussenegger/nvim-dap",
-    config = function()
-        require("dap").setup()
-        require("dap").listeners.after.event_initialized["dapui_config"] = require("dapui").open
-        require("dap").listeners.before.event_terminated["dapui_config"] = require("dapui").close
-        require("dap").listeners.before.event_exited["dapui_config"] = require("dapui").close
-    end,
     keys = {
-        {
-            "<F5>",
-            function()
-                require("dap").continue()
-            end,
-            mode = "n",
-            desc = "Debug continue",
-        },
-        {
-            "<F1>",
-            function()
-                require("dap").step_into()
-            end,
-            mode = "n",
-            desc = "Debug step into",
-        },
-        {
-            "<F2>",
-            function()
-                require("dap").step_over()
-            end,
-            mode = "n",
-            desc = "Debug step over",
-        },
-        {
-            "<F3>",
-            function()
-                require("dap").step_out()
-            end,
-            mode = "n",
-            desc = "Debug step out",
-        },
-        {
-            "<leader>b",
-            function()
-                require("dap").toggle_breakpoint()
-            end,
-            mode = "n",
-            desc = "Debug toggle breakpoint",
-        },
-        {
-            "<leader>B",
-            function()
-                require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-            end,
-            mode = "n",
-            desc = "Debug toggle breakpoint with condition",
-        },
+        -- stylua: ignore start
+        { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "[d]ebug toggle [b]reakpoint" },
+        { "<leader>dc", function() require("dap").continue() end,          desc = "[d]ebug [c]ontinue" },
+        { "<Down>",     function() require("dap").step_into() end,         desc = "debug step into" },
+        { "<Up>",       function() require("dap").step_out() end,          desc = "debug step out" },
+        { "<Right>",    function() require("dap").step_over() end,         desc = "debug step Over" },
+        { "<leader>dt", function() require("dap").terminate() end,         desc = "[d]ebug [t]erminate" },
+        -- stylua: ignore end
     },
-    {
-        "rcarriga/nvim-dap-ui",
-        opts = {
-            icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-            controls = {
-                icons = {
-                    pause = "⏸",
-                    play = "▶",
-                    step_into = "⏎",
-                    step_over = "⏭",
-                    step_out = "⏮",
-                    step_back = "b",
-                    run_last = "▶▶",
-                    terminate = "⏹",
+    dependencies = {
+        {
+            "rcarriga/nvim-dap-ui",
+            dependencies = { "theHamsta/nvim-dap-virtual-text" },
+            opts = {
+                icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+                controls = {
+                    icons = {
+                        pause = "⏸",
+                        play = "▶",
+                        step_into = "⏎",
+                        step_over = "⏭",
+                        step_out = "⏮",
+                        step_back = "b",
+                        run_last = "▶▶",
+                        terminate = "⏹",
+                    },
                 },
-             },
-        }
-    },
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            automatic_setup = true,
+            },
+        },
+        "nvim-neotest/nvim-nio",
+        {
+            "jay-babu/mason-nvim-dap.nvim",
+            dependencies = { "mason.nvim" },
+            opts = {
+                automatic_setup = true,
+                ensure_installed = {
+                    "delve",
+                },
+            },
+        },
 
-            ensure_installed = {
-                "delve",
-             },
-        }
+        -- Add your own debuggers here
+        "leoluz/nvim-dap-go",
     },
-    "nvim-neotest/nvim-nio",
-    "jay-babu/mason-nvim-dap.nvim",
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
 
-    -- Add your own debuggers here
-    "leoluz/nvim-dap-go",
+        dap.listeners.before.attach.dapui_config = dapui.open
+        dap.listeners.after.event_initialized.dapui_config = dapui.open
+        dap.listeners.before.event_terminated.dapui_config = dapui.close
+        dap.listeners.before.event_exited.dapui_config = dapui.close
+    end,
 }
